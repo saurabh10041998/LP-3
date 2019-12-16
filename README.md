575e010ebaf1541a9ea832c0f955ab8efa4089a9
saurabh10041998 <shindesaurabhpict@gmail.com>
2019-12-16 18:31:10 +0530

    generation of data

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # LP-3 assignment
 
 This is the all the implemented LP-3 assignment for <b>Machine Learning</b>
+
+
+DES
+---
+	56 bit of key.
+	64 bit of plain text.
+	
+	initial len(key) = 64 -> 56(0,8,16,32..)
+	
--- ics/assignment1/des.py
+++ ics/assignment1/des.py
@@ -1,6 +1,21 @@
-import doctest
+import random
+import time
 
+# set the random seed
+random.seed(time.time())
 
 if __name__ == "__main__":
-	print("normal placeholder")
+	# generation of the initial data 
+  	hash  = random.getrandbits(65)
+	plain_text = "{0:b}".format(hash)
+	key = "{0:b}".format(random.getrandbits(65))
+	
+	#generation of key
+	key = key[::-1]
+	print(len(key))
+	new_key = ""
+	for i in range(8):
+	  	new_key += key[8 * i : 8 * (i+1) -1]
+	print(len(new_key))
+	
 
