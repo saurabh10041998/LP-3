d4ad7857cc439c9ef77e49e1c14976cda58fc071
saurabh10041998 <shindesaurabhpict@gmail.com>
2019-12-18 18:12:19 +0530

    input data generation

--- README.md
+++ README.md
@@ -5,8 +5,21 @@ This is the all the implemented LP-3 assignment for <b>Machine Learning</b>
 
 DES
 ---
-	56 bit of key.
-	64 bit of plain text.
-	
-	initial len(key) = 64 -> 56(0,8,16,32..)
+* 56 bit of key.
+* 64 bit of plain text.
+* initial len(key) = 64 -> 56(0,8,16,32..)
+* two main mechanism
+	* substitution
+	* transposition
+* Initial permutation( IP )
+
+ 
+## S-DES
+* simplified DES
+* 8 bit plain-text/cipher-text
+* 10 bits key size of encryption
+* two rounds
+
+
+
 	
--- ics/assignment1/des.py
+++ ics/assignment1/des.py
@@ -1,21 +1,35 @@
+#!/usr/bin/python
 import random
 import time
 
 # set the random seed
 random.seed(time.time())
 
+# p10 table(custom??)
+p10 = {
+	"input": [1 ,2, 3, 4, 5, 6, 7, 8, 9, 10],
+	"output" : [3, 5, 7, 4, 10, 1, 9, 8, 6]
+}
+
+def KeyTrasform(key):
+	pass
+
+def pad(st, length, val):
+	st = val*(length - len(st)) + st
+	return st
+
+
+
 if __name__ == "__main__":
 	# generation of the initial data 
-  	hash  = random.getrandbits(65)
-	plain_text = "{0:b}".format(hash)
-	key = "{0:b}".format(random.getrandbits(65))
-	
-	#generation of key
-	key = key[::-1]
-	print(len(key))
-	new_key = ""
-	for i in range(8):
-	  	new_key += key[8 * i : 8 * (i+1) -1]
-	print(len(new_key))
+  	text = random.getrandbits(8)
+	plain_text = "{0:b}".format(text)
+	plain_text = pad(plain_text, 8, "1") 
+	text = random.getrandbits(10)
+	key = "{0:b}".format(text)
+	key  = pad(key, 10, "1")
+	print(plain_text)
+	print(key)
+		
 	
 
