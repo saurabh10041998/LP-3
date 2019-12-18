f6c2a7ad8aa0685de8792b79988c48a6a79b74b2
saurabh10041998 <shindesaurabhpict@gmail.com>
2019-12-18 18:37:06 +0530

    generation of K1

--- ics/assignment1/des.py
+++ ics/assignment1/des.py
@@ -8,11 +8,36 @@ random.seed(time.time())
 # p10 table(custom??)
 p10 = {
 	"input": [1 ,2, 3, 4, 5, 6, 7, 8, 9, 10],
-	"output" : [3, 5, 7, 4, 10, 1, 9, 8, 6]
+	"output" : [3, 5, 2, 7, 4, 10, 1, 9, 8, 6]
 }
 
-def KeyTrasform(key):
-	pass
+#p8 table(custom??)
+p8 = {
+	"input": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+	"output": [6, 3, 7, 4, 8, 5, 10, 9] 
+}
+
+
+def rotate(input, d):
+	Lfirst = input[0: d]
+	Lsecond = input[d: ]
+	return Lsecond + Lfirst
+
+def KeyTransform(key):
+	n = len(key)
+	lkey = key[:n//2]
+	rkey = key[n//2: ]
+	lkey = rotate(lkey,1)
+	rkey = rotate(rkey,1)
+	key = lkey + rkey
+	# apply p8 permutation
+	new_key = [0] * 8 
+	i = 0
+	for j in p8["output"]:
+		new_key[i] = key[j-1]
+		i += 1
+	return "".join(new_key)
+	
 
 def pad(st, length, val):
 	st = val*(length - len(st)) + st
@@ -28,8 +53,8 @@ if __name__ == "__main__":
 	text = random.getrandbits(10)
 	key = "{0:b}".format(text)
 	key  = pad(key, 10, "1")
-	print(plain_text)
-	print(key)
+	K1 = KeyTransform(key)
+	print(K1)
 		
 	
 
